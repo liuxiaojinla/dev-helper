87e0d4e55d395e2a108b9391d95b0695f745a0df
lxsea <1540175452@qq.com>
2018-04-09 18:30:56 +0800

    inited

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,10 +8,10 @@ grunt.config.init({
 			authors: '北斗 <657306123@qq@qq.com>', // 作者
 			projectUrl: '',
 			appDirectory: './packager/electron-win32-x64', // 要打包的输入目录
-			outputDirectory: './package_dir', // grunt打包后的输出目录
+			outputDirectory: './packager', // grunt打包后的输出目录
 			exe: 'electron.exe', // 生成的exe文件
-			description: 'My Superb Vue Project For Electron',
-			setupIcon: './app/hots.ico', // 图标
+			description: 'Electron与Vue整合',
+			setupIcon: './dist/icons/icon.ico', // 图标
 			noMsi: true // 是否生成.msi
 		}
 	}
