38a31421d96d55fde8610493cec737914836c21d
lxsea <1540175452@qq.com>
2018-09-30 15:26:10 +0800

    升级vue

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,192 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+	<Layout class="layout">
+		<Header class="layout-header-bar">
+			<div class="layout-header-left">
+				<div class="layout-action-btn">
+					<Icon type="ios-arrow-left" size="24" @click.native="onBack"></Icon>
+				</div>
+				<div class="layout-action-btn">
+					<Icon type="ios-reload" size="24" @click.native="onRefresh"></Icon>
+				</div>
+			</div>
+			<div class="layout-header-center">
+				{{title}}
+			</div>
+			<div class="layout-header-right">
+				<div class="layout-action-btn">
+					<Icon type="ios-close-empty" size="24" @click.native="onClose" v-show="isClosable"></Icon>
+				</div>
+				<div class="layout-action-btn">
+					<Icon :type="maxIcon" size="24" @click.native="onMax" v-show="isMaximizable"></Icon>
+				</div>
+				<div class="layout-action-btn">
+					<Icon type="ios-minus-empty" size="24" @click.native="onMinni" v-show="isMinimizable"></Icon>
+				</div>
+				<Dropdown trigger="click" placement="bottom-end" :transfer="true" @on-click="onMenuSelect">
+					<div class="layout-action-btn">
+						<Icon type="ios-more" size="24"></Icon>
+					</div>
+					<DropdownMenu slot="list">
+						<DropdownItem name="debug">
+							<Icon type="bug" size="20"></Icon>
+							开启调试模式
+						</DropdownItem>
+					</DropdownMenu>
+				</Dropdown>
+			</div>
+		</Header>
+		<Content class="layout-content">
+			<transition :name="transitionName" mode="out-in">
+				<router-view/>
+			</transition>
+		</Content>
+	</Layout>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+<script>
+	export default {
+		name: 'App',
+		win: null,
+		data() {
+			const win = this.$options.win = sys.getCurrentWindow();
+			return {
+				transitionName: '',
+				title: '',
+				isMaximize: false,
+				isMaximizable: win.isMaximizable(),
+				isMinimizable: win.isMinimizable(),
+				isClosable: win.isClosable(),
+			}
+		},
+		computed: {
+			maxIcon() {
+				return this.isMaximize ? 'ios-browsers' : 'ios-browsers-outline';
+			}
+		},
+		created() {
+			this.$nextTick(this.updateTitle);
+			const win = this.$options.win;
+			win.on('maximize', () => this.isMaximize = true);
+			win.on('unmaximize', () => this.isMaximize = false);
+		},
+		methods: {
+			onBack() {
+				sys.navigateBack();
+			},
+			onRefresh() {
+				window.location.reload();
+			},
+			onMenuSelect(name) {
+				if ('debug' === name) {
+					const win = this.$options.win;
+					this.isDebug = !win.webContents.isDevToolsOpened();
+					win.webContents.openDevTools();
+				} else {
+				}
+			},
+			onMinni() {
+				this.$options.win.minimize();
+			},
+			onMax() {
+				const win = this.$options.win;
+				this.isMaximize ? win.restore() : win.maximize();
+				this.isMaximize = !this.isMaximize;
+			},
+			onClose() {
+				this.$options.win.close();
+			},
+			updateTitle() {
+				this.title = this.$route.meta.title;
+			},
+		},
+		watch: {
+			'$route'(to, from) {
+				const toDepth = to.path.split('/').length;
+				const fromDepth = from.path.split('/').length;
+				this.transitionName = toDepth < fromDepth ? 'zoom' : 'fade';
+				this.$nextTick(this.updateTitle);
+			}
+		}
+	}
+</script>
+
+<style>
+	body {
+		background-color: transparent;
+	}
+
+	.layout {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+
+		position: relative;
+		overflow: hidden;
+		height: 100vh;
+	}
+
+	.layout-header-bar {
+		display: table;
+		table-layout: fixed;
+		width: 100%;
+		border-collapse: collapse;
+		border: 0;
+	}
+
+	.layout-header-left,
+	.layout-header-center,
+	.layout-header-right {
+		display: table-cell;
+		border: 0;
+		vertical-align: top;
+		padding: 0 5px;
+		white-space: nowrap;
+		overflow: hidden;
+	}
+
+	.layout-header-left {
+		width: 84px;
+	}
+
+	.layout-header-center {
+		text-overflow: ellipsis;
+		font-size: 18px;
+		line-height: 48px;
+		-webkit-app-region: drag;
+	}
+
+	.layout-header-right {
+		width: 156px;
+	}
+
+	.layout-header-right > div {
+		float: right;
+	}
+
+	.layout-action-btn {
+		display: inline-block;
+		padding-left: 10px;
+		padding-right: 10px;
+
+		line-height: 48px;
+		transition: all 0.1s;
+
+		user-select: none;
+		-webkit-user-select: none;
+	}
+
+	.layout-action-btn > .ivu-icon {
+		vertical-align: middle;
+	}
+
+	.layout-action-btn:active {
+		opacity: 0.3;
+	}
+
+	.layout-content {
+		position: relative;
+		overflow-x: hidden;
+		overflow-y: auto;
+		/*background-color: white;*/
+	}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,66 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import iView from 'iview';
 
-Vue.config.productionTip = false
+import './assets/less/app.less';
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+Vue.use(iView);
+Vue.use(function(Vue, options) {
+	window.sys = sys;
+	sys.__ROUTER__ = router;
+	sys.request.addResponseInterceptor((res) => {
+		if (res && res.data) {
+			const data = res.data;
+			if (data.data || data.result) return data;
+
+			const err = new Error(data.msg);
+			err.response = res;
+			err.data = data;
+			throw err;
+		} else {
+			const err = new Error('请求服务器失败，请联系管理员~');
+			err.response = res;
+			throw err;
+		}
+	}, (err) => {
+		return Promise.reject(err);
+	});
+
+	//上传失败提示信息
+	function showUploadError(msg) {
+		sys.showNotification({
+			title: '错误提示',
+			type: 'error',
+			desc: `上传失败：${msg}`,
+		});
+	}
+
+	Vue.mixin({
+		methods: {
+			//上传格式错误
+			onUploadFormatError: () => showUploadError('文件格式错误'),
+			//上传大小错误
+			onUploadSizeError: () => showUploadError('文件大小超出限制'),
+		}
+	});
+});
+
+// 页面打开后的操作
+router.afterEach(function(route) {
+	iView.LoadingBar.finish();
+	if (route.meta && route.meta.title) {
+		document.title = route.meta.title;
+	}
+});
+
+Vue.config.productionTip = false;
+
+window.IS_DEV = process.env.NODE_ENV === 'development';
+window.__ROUTER__ = router;
+window.__VUE__ = new Vue({
+	router,
+	store,
+	render: h => h(App)
+}).$mount('#app');
